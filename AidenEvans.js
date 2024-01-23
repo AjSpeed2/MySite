e61f7ae745d1631fb5e8af35e9c907cf2138f2b8
Aiden Evans <aidenjevans@hotmail.com>
2024-01-22 20:27:54 -0800

    Redesigned

--- AidenEvans.js
+++ AidenEvans.js
@@ -1,1 +1,9 @@
-window.scrollTo(0, 0);
+
+sessionStorage.setItem("active", "false");
+let active = sessionStorage.getItem("active");
+if (!active) {
+  window.scrollTo(0, 0);
+  active = true;
+  sessionStorage.setItem("active", true);
+}
+
